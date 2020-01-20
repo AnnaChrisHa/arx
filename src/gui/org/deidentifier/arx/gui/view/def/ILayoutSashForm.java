7901eaf1deced86ecfb323ce9744449270d82f38
Anna Haber <AHaber@kabelmail.de>
2020-01-20 16:07:06 +0100

    New interface ILayoutSashForm included in the gui.view.def-package.
    Change-Id: I587716062a9709c98f77fbb8f9f413caa2d219c9
    Signed-off-by: Anna Haber <AHaber@kabelmail.de>

---
+++ src/gui/org/deidentifier/arx/gui/view/def/ILayoutSashForm.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.deidentifier.arx.gui.view.def;
+
+/**
+ * An interface for SashForm-Layouts 
+ * (to be used in layouts for masking, for example)
+ * 
+ * @author AHabe
+ *
+ */
+public interface ILayoutSashForm {
+
+}
